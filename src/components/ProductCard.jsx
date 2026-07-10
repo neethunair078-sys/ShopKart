038b6a9f01052f10f899c0802f3c2c54b904cd39
Neethu Nair <neethunair078@gmail.com>
2026-07-10 16:12:00 +0530

    conditional change on add to cart button

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import '../css/card.css'
 import StarRating from './StarRating'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { addToCart } from '../redux/slices/cartSlice'
 import { HiOutlineShoppingCart } from "react-icons/hi2";
 import toast from 'react-hot-toast'
+import { Link } from 'react-router-dom'
 
 
 const ProductCard = ({item, thumbnail, title, price, rating,reviews}) => {
     const dispatch = useDispatch()
+
+    const cartItem = useSelector((state) => state.cart.cartItems)
+
+
+
+    const isAdded = cartItem.find(cartItem => cartItem.id === item.id)
 
     const handleAddToCart = () => {
         dispatch(addToCart(item));
@@ -21,10 +28,21 @@ const ProductCard = ({item, thumbnail, title, price, rating,reviews}) => {
             <h2 className='text-md font-bold'>{title}</h2>
             <StarRating rating={rating} reviews={reviews} />
             <p className='font-bold'>${price}</p>
-            <button  className='btn-primary flex items-center justify-center gap-2' onClick={handleAddToCart}>
+            
+
+            {!isAdded ? 
+              <button  className='btn-primary flex items-center justify-center gap-2' onClick={handleAddToCart}>
                 <HiOutlineShoppingCart size={20} className='logo-icon' /> 
-                Add to Cart
-            </button>
+                Add to cart
+            </button>  
+
+            :
+
+            <Link to="/cart" className='btn-primary flex items-center justify-center gap-2'>
+                <HiOutlineShoppingCart size={20} className='logo-icon' /> 
+                Go to cart 
+            </Link>  
+            }
         </div>
     </>
   )
