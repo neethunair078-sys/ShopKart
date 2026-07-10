8c571fceea3da7a9ae2f3cbe14c469774e7081c3
dinuaj <dinuaj10@gmail.com>
2026-07-10 15:16:46 +0530

    created cart page and added it to the route path

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import MainLayout from './Layout/MainLayout'
 import ProductList from './pages/ProductList'
+import Cart from './pages/Cart'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path='/' element={<ProductList/>}/>
+          <Route path='/cart' element={<Cart />} />
         </Route>
       </Routes>
      </BrowserRouter>
