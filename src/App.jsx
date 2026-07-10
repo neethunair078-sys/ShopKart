d0973687f6794b71351ec0841ef391c28b7ecf29
salith <salithsurendran@gmail.com>
2026-07-10 16:24:06 +0530

    created a page for login

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import MainLayout from './Layout/MainLayout'
 import ProductList from './pages/ProductList'
+import Cart from './pages/Cart'
+import { Provider } from 'react-redux'
+import Login from './pages/login'
 
 function App() {
 
   return (
     <>
-     <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path='/' element={<ProductList/>}/>
-        </Route>
-      </Routes>
-     </BrowserRouter>
+        <BrowserRouter>
+          <Routes>
+            <Route path="login" element={<Login/>}/>
+            <Route element={<MainLayout />}>
+              <Route path='/' element={<ProductList />} />
+              <Route path='/cart' element={<Cart />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
     </>
   )
 }
