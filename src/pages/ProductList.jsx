2a29a2a7530eb576a6fb6e109a8b44210580182c
Neethu Nair <neethunair078@gmail.com>
2026-07-13 13:49:53 +0530

    added a static link to product details page from product listing

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -21,7 +21,7 @@ useEffect(() => {
 
   return (
     <>
-        <div className='grid grid-cols-4 gap-2'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2'>
             {products.map((item) => (
                 <ProductCard 
                     item={item}
