dbeaf32cc863fb88c90e676443b61bf939e0d3f3
salith <salithsurendran@gmail.com>
2026-07-14 16:23:15 +0530

    Register UI

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,15 +5,15 @@ import { useState } from "react";
 function Register() {
     const [name, setName] = useState("");
 
-    const [email,setEmail] = useState("");
+    const [email, setEmail] = useState("");
 
     const [password, setPassword] = useState("");
 
-    const [confirmPassword, setConfirmPassword]= useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
-    function handleRegister(){
+    function handleRegister() {
         console.log("Register")
-        
+
         if (name === "") {
             alert("Name is Required")
             return;
@@ -30,32 +30,91 @@ function Register() {
             alert("confirm ypur Password")
             return;
         }
-        if (password !== confirmPassword){
+        if (password !== confirmPassword) {
             alert("Passwords do not match")
             return;
         }
-         alert("Registration Successful");
+        alert("Registration Successful");
     }
 
-    
+
     return (
+
         <>
 
-            <h1>Register</h1>
-
-            <input type="name" placeholder="Enter Name" value={name} onChange={(e)=>setName(e.target.value)}/>
-            <input type="email" placeholder="Enter Email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-            <input type="password" placeholder="Enter Password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
-            <input type="password" placeholder="Enter Password" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)}/>
-
-            <button onClick={handleRegister}>Register</button>
+            <div className="min-h-screen bg-gray-100">
 
 
-               
-                
-            
+                <div className="flex justify-center">
 
+                    <div>
+                        <h1 className="text-5xl font-bold mb-4">
+                            Create Account
+                        </h1>
+
+                        <p className="text-gray-500 text-xl mb-10">
+                            Register to enjoy a seamless shopping experience.
+                        </p>
+                    </div>
+
+
+                    <div>
+                        <div className="bg-white shadow-xl rounded-2xl p-12">
+                            <h1>Register</h1>
+                            <label className="font-semibold">Full Name</label>
+                            <input type="name" placeholder="Enter Name" className="w-full border border-gray-300 rounded-lg px-4 py-3 mt-2 mb-5 focus:outline-none focus:ring-2 focus:ring-blue-500" value={name} onChange={(e) => setName(e.target.value)} />
+                            <label className="font-semibold">Email</label>
+                            <input type="email" placeholder="Enter Email" className="w-full border border-gray-300 rounded-lg px-4 py-3 mt-2 mb-5 focus:outline-none focus:ring-2 focus:ring-blue-500" value={email} onChange={(e) => setEmail(e.target.value)} />
+                            <label className="font-semibold">Password</label>
+                            <input type="password" placeholder="Enter Password" className="w-full border border-gray-300 rounded-lg px-4 py-3 mt-2 mb-5 focus:outline-none focus:ring-2 focus:ring-blue-500" value={password} onChange={(e) => setPassword(e.target.value)} />
+                            <label className="font-semibold">Confirm Password</label>
+                            <input type="password" placeholder="Enter Password" className="w-full border border-gray-300 rounded-lg px-4 py-3 mt-2 mb-5 focus:outline-none focus:ring-2 focus:ring-blue-500" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+
+                            <button className="w-full bg-blue-600 hover:bg-blue-700 text-white py-3 rounded-lg font-semibold transition" onClick={handleRegister}>Register</button>
+                        </div>
+                    </div>
+                </div>
+
+                <p className="text-center mt-6">Already have an account?
+                    <span className="text-blue-600 cursor-pointer ml-2">
+                        Login
+                    </span>
+                </p>
+                <div>
+                    <div className="grid md:grid-cols-3 gap-10 bg-white mt-16 py-8 shadow">
+
+                        <div className="text-center">
+                            <h3 className="font-bold text-lg">
+                                Secure Shopping
+                            </h3>
+                            <p className="text-gray-500">
+                                100% secure and protected
+                            </p>
+                        </div>
+
+                        <div className="text-center">
+                            <h3 className="font-bold text-lg">
+                                Fast Delivery
+                            </h3>
+                            <p className="text-gray-500">
+                                Get your products quickly
+                            </p>
+                        </div>
+
+                        <div className="text-center">
+                            <h3 className="font-bold text-lg">
+                                24/7 Support
+                            </h3>
+                            <p className="text-gray-500">
+                                We're here anytime
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </>
+
     )
 };
 
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -27,7 +27,7 @@ function Login() {
             <div className=" w-screen h-screen flex justify-center items-center ">
                 <div className="w-150 h-100 bg-blue-50 rounded-2xl flex flex-col justify-center items-center gap-6">
                     <h2 className="font-bold">Login</h2>
-                    <input className="" type="email" placeholder="Enter Email"  className="w-80 h-10 bg-white rounded-lg p-2 outline-2 outline-offset-2" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input type="email" placeholder="Enter Email"  className="w-80 h-10 bg-white rounded-lg p-2 outline-2 outline-offset-2" value={email} onChange={(e) => setEmail(e.target.value)} />
                     <input type="Password" placeholder="Enter Password"  className="w-80 h-10 bg-white rounded-lg p-2 outline-2 outline-offset-2"  value={password} onChange={(e) => setPassword(e.target.value)} />
                     <button onClick={handleLogin}>Login</button>
                 </div>
