bba5ae1a05df6e68e1cbee7d7c6dd184f869504d
Krishnanand K <kookalkrishnanand@gmail.com>
2026-07-15 16:51:03 +0530

    Checkout Corrections

---
+++ src/redux/slices/orderslice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    orders: []
+};
+
+const orderSlice = createSlice({
+    name: "order",
+    initialState,
+    reducers: {
+        addOrder: (state, action) => {
+            state.orders.push(action.payload);
+        }
+    }
+});
+
+export const { addOrder } = orderSlice.actions;
+
+export default orderSlice.reducer;
