de054ece951e320fd4533a1ce4b96c9dff91f21f
Neethu Nair <neethunair078@gmail.com>
2026-07-15 12:12:13 +0530

    updated the logic of navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import { HiOutlineShoppingCart } from "react-icons/hi2";
+import { HiOutlineShoppingCart, HiOutlineUser } from "react-icons/hi2";
 import '../css/navbar.css'
-import { Link, useNavigate } from 'react-router-dom';
-import { useSelector } from "react-redux";
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
 import toast from 'react-hot-toast';
+import { clearCart } from '../redux/slices/cartSlice';
 
 
 const Navbar = () => {
+  const dispatch = useDispatch()
+  const location = useLocation()
+  const navigate = useNavigate()
+
   const cartItems = useSelector((state) => state.cart.cartItems)
 
   const user = JSON.parse(localStorage.getItem("user"));
 
-  const navigate = useNavigate()
 
 
   const totalItems = cartItems.reduce(
@@ -19,13 +23,16 @@ const Navbar = () => {
     0
   );
 
+  const hideNav = location.pathname === "/login" || location.pathname === '/register'
 
 
-const handleLogout = () => {
-  localStorage.removeItem("user");
-  toast.success("Logout successfully")
-  navigate('/')
-}
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    dispatch(clearCart())
+    toast.success("Logout successfully")
+    navigate('/')
+  }
 
 
 
@@ -40,32 +47,35 @@ const handleLogout = () => {
         </Link>
 
         <div className='flex gap-4 items-center'>
+          {user ?
+            <>
+              <p><HiOutlineUser size={20} className="inline text-gray-900" /> {user.name}</p>
+              <button onClick={handleLogout} className='cursor-pointer bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-2 border border-gray-400 rounded shadow'>
+                Logout
+              </button>
+            </>
 
-          <p>{user.name}</p>
+            :
 
-          {user ? 
-            <button onClick={handleLogout} className='cursor-pointer bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-2 border border-gray-400 rounded shadow'>
-            Logout
-          </button>
+            <button onClick={() => navigate('/login')} className='cursor-pointer bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-2 border border-gray-400 rounded shadow'>
+              Login
+            </button>
 
-          :
-
-          <button onClick={() => navigate('/login')} className='cursor-pointer bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-2 border border-gray-400 rounded shadow'>
-            Login
-          </button>
-          
           }
-          
 
-          <Link to="/cart">
-            <HiOutlineShoppingCart size={24} className='logo-icon' />
-            {totalItems > 0 && (
-              <span className="absolute top-3 right-3 bg-red-500 text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
-                {totalItems}
-              </span>
-            )}
 
-          </Link>
+          {!hideNav &&
+
+            <Link to="/cart">
+              <HiOutlineShoppingCart size={24} className='logo-icon' />
+              {totalItems > 0 && (
+                <span className="absolute top-3 right-3 bg-red-500 text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
+                  {totalItems}
+                </span>
+              )}
+
+            </Link>
+          }
         </div>
       </div>
     </>
